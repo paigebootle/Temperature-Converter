ca93cae9cbc4d797dd21b3f89f4808512729fefa
paigebootle <paigeboo@pnghs.school.nz>
2022-03-18 14:57:59 +1300

    Create help GUI 1.py

---
+++ help GUI 1.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random
+
+class Converter:
+    def __init__(self, parent):
+        print("Hello World")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter(root)
+    root.mainloop()
